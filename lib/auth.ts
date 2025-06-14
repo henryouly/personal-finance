ebfa3d22f2c4d4b3d8a4b9ec6f9b1bb1662668bb
Henry Ou <henryouly@gmail.com>
2025-06-14 09:50:17 -0700

    Enable email and password authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,5 +5,8 @@ import { db } from "@/db/db";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-  })
+  }),
+  emailAndPassword: {
+    enabled: true
+  },
 });
